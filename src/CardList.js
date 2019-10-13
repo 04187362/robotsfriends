52787c122bbb09da806749ccb0d983ec21d438a4
Francis <francisseneve@gmail.com>
2019-10-13 23:24:26 +0200

    CardList update

--- src/CardList.js
+++ src/CardList.js
@@ -7,7 +7,14 @@ const CardList = ({robots})=>{
     <div>
           {
             robots.map((user,index)=>{
-             return (<Card key={index} id={robots[index].id} name={robots[index].name} email={robots[index].email}/>);
+             return (
+               <Card
+               key={index}
+               id={robots[index].id}
+               name={robots[index].name}
+               email={robots[index].email}
+               />
+             );
            })
           }
     </div>
