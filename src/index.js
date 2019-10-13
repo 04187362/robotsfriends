5f747e4daa5a3fcc4f01dd153c87b6ad2abc18bd
Francis <francisseneve@gmail.com>
2019-10-13 23:22:56 +0200

    Add the CardList component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './Card';
+import CardList from './CardList';
 import {robots} from './robots';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(
-  <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-        <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-        <Card id={robots[4].id} name={robots[4].name} email={robots[4].email}/>
-  </div>,document.getElementById('root'));
+ReactDOM.render(<CardList robots={robots}/>,document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
