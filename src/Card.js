e41218af0fec3d3e8c665fdbc795fc622c50245d
Francis <francisseneve@gmail.com>
2019-10-13 22:49:28 +0200

    The first step with the robots friends app

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Card = ({name,email,id})=>{
+  return (
+    <div className="tc bg-light-green dib br3 pa3 ma3 grow bw3 shadow-5">
+        <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
+        <div>
+              <h2>{name}</h2>
+              <p>{email}</p>
+        </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Card from './Card';
+import {robots} from './robots';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <div>
+        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+        <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
+        <Card id={robots[4].id} name={robots[4].name} email={robots[4].email}/>
+  </div>,document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
